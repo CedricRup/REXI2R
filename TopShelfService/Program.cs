d2fcb94980152939c5ccbb3f3e6eb66d464da89a
Cedric Rup <cedric.rup@gmail.com>
2013-07-03 15:23:09 +0200

    Simple TopShelf service

--- TopShelfService/Program.cs
+++ TopShelfService/Program.cs
@@ -1,14 +1,41 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
+using System.Timers;
+using Topshelf;
 
 namespace TopShelfService
 {
-    class Program
+
+    public class TownCrier
     {
-        static void Main(string[] args)
+        readonly Timer _timer;
+        public TownCrier()
         {
+            _timer = new Timer(1000) { AutoReset = true };
+            _timer.Elapsed += (sender, eventArgs) => Console.WriteLine("It is {0} an all is well", DateTime.Now);
+        }
+        public void Start() { _timer.Start(); }
+        public void Stop() { _timer.Stop(); }
+    }
+
+    public class Program
+    {
+        public static void Main()
+        {
+            HostFactory.Run(x =>
+            {
+                x.Service<TownCrier>(s =>
+                {
+                    s.ConstructUsing(name => new TownCrier());
+                    s.WhenStarted(tc => tc.Start());
+                    s.WhenStopped(tc => tc.Stop());
+                });
+                x.RunAsLocalSystem();
+
+                x.SetDescription("Sample Topshelf Host");
+                x.SetDisplayName("Stuff");
+                x.SetServiceName("stuff");
+            });
         }
     }
 }
+
