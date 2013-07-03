4a46e6b58c7e147c8ccaec9fccff4551a59a4f81
Cedric Rup <cedric.rup@gmail.com>
2013-07-03 18:14:06 +0200

    Simple Api

---
+++ I2RRexApi/User.cs
@@ -0,0 +1,11 @@
+﻿namespace I2RRexApi
+{
+    public class User   
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+    }
+
+    
+}
---
+++ I2RRexApi/UserService.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections.Generic;
+using System.Net;
+
+namespace I2RRexApi
+{
+    public class UserService
+    {
+        public UserService()
+        {
+            
+        }
+
+        public IEnumerable<User> GetUsers()
+        {
+            var client = new WebClient();
+            var response = client.DownloadString("http://localhost:8080/Users");
+            return Newtonsoft.Json.JsonConvert.DeserializeObject<IEnumerable<User>>(response);
+        }
+    }
+}
