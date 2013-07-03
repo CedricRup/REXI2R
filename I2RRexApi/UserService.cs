368c6877efca8c8761be6d3e773d864671111a9e
Cedric Rup <cedric.rup@gmail.com>
2013-07-03 18:27:47 +0200

    With conf

--- I2RRexApi/UserService.cs
+++ I2RRexApi/UserService.cs
@@ -1,20 +1,34 @@
 ﻿using System.Collections.Generic;
+using System.Configuration;
 using System.Net;
 
 namespace I2RRexApi
 {
     public class UserService
     {
-        public UserService()
+        private readonly APIConfig config ;
+
+        public UserService(APIConfig config)
         {
-            
+            this.config = config;
         }
 
         public IEnumerable<User> GetUsers()
         {
             var client = new WebClient();
-            var response = client.DownloadString("http://localhost:8080/Users");
+            var response = client.DownloadString(config.MainUrl + "/Users");
             return Newtonsoft.Json.JsonConvert.DeserializeObject<IEnumerable<User>>(response);
+        }
+    }
+
+    public class APIConfig : ConfigurationSection
+    {
+
+        [ConfigurationProperty("MainUrl")]
+        public string MainUrl
+        {
+            get { return this["MainUrl"] as string; }
+            set { this["MainUrl"] = value; }
         }
     }
 }
